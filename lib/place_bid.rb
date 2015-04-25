de87b4b5dccdb5ce0ab91d6ef17d6b72c2e723bf
Thuong Ho <thuong.t.ho@gmail.com>
2015-04-25 14:55:45 -0700

    create a test to test bid value under current bid

--- lib/place_bid.rb
+++ lib/place_bid.rb
@@ -9,12 +9,21 @@ class PlaceBid
   def execute
     # find the auction by the auction id
     auction = Auction.find @auction_id
+
+    # prevent the bid object to be created if value is less than current bid
+    if @value <= auction.current_bid
+      return false
+    end
+
     # instantiate a new bid using the value and user id
     # a new bid that is build around the auction and user id
     bid = auction.bids.build value: @value, user_id: @user_id
 
     if bid.save
       return true
+    else 
+      # return false in case cannot save
+      return false
     end
   end
 end
--- test/lib/place_bid_test.rb
+++ test/lib/place_bid_test.rb
@@ -3,21 +3,44 @@ require "test_helper"
 require "place_bid"
 
 class PlaceBidTest < MiniTest::Test
-  def test_it_places_a_bid
-    user = User.create! email: "sample@awktion.com", password: "password"
-    another_user = User.create! email: "another_user@awktion.com", password: "password"
-    product = Product.create! name: "Some Product"
-    auction = Auction.create! value: 10, product_id: product.id
+  def setup
+    @user = User.create! email: "sample@awktion.com", password: "password"
+    @another_user = User.create! email: "another_user@awktion.com", password: "password"
+    @product = Product.create! name: "Some Product"
+    @auction = Auction.create! value: 10, product_id: product.id
+  end
 
+  def test_it_places_a_bid
     # placing the bid
     # instantiate a new service to place bid
     # insert a new bid value, attach to the user id of another user, and add auction id
     # value, user_id, auction_id needs to be defined in lib/place_bid.rb
-    service = PlaceBid.new value: 11, user_id: another_user, auction_id: auction.id
+    service = PlaceBid.new(
+      value: 11, 
+      user_id: another_user.id, 
+      auction_id: auction.id
+    )
 
     service.execute
 
     # let's say we want to bid 1 dollar above the auction value
     assert_equal 11, auction.current_bid
   end
+
+  def test_fails_to_place_bid_under_current_value
+    service = PlaceBid.new(
+      value: 9,
+      user_id: another_user.id,
+      auction_id: auction.id
+    )
+
+    # assert false
+    # refute: fails if a test is true value
+    refute service.execute, "Your bid is below the current bid."
+  end
+
+  private
+
+  # make sure the variables in setup are publicly avail
+  attr_reader :user, :another_user, :product, :auction
 end
