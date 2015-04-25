14382c95b968cc37c733b18c4600aefa5f233de2
Thuong Ho <thuong.t.ho@gmail.com>
2015-04-25 14:32:42 -0700

    add test to test a bid at a higher value than current bid

--- lib/place_bid.rb
+++ lib/place_bid.rb
@@ -1,3 +1,20 @@
 # a class to make sure that new bids are not less than current bid
 class PlaceBid
+  def initialize options
+    @value = options[:value]
+    @user_id = options[:user_id]
+    @auction_id = options[:auction_id]
+  end
+
+  def execute
+    # find the auction by the auction id
+    auction = Auction.find @auction_id
+    # instantiate a new bid using the value and user id
+    # a new bid that is build around the auction and user id
+    bid = auction.bids.build value: @value, user_id: @user_id
+
+    if bid.save
+      return true
+    end
+  end
 end
--- test/lib/place_bid_test.rb
+++ test/lib/place_bid_test.rb
@@ -1,8 +1,10 @@
 require "test_helper"
+# need to use the library of lib/place_bid.rb
+require "place_bid"
 
 class PlaceBidTest < MiniTest::Test
   def test_it_places_a_bid
-    user = User.create! email "sample@awktion.com", password: "password"
+    user = User.create! email: "sample@awktion.com", password: "password"
     another_user = User.create! email: "another_user@awktion.com", password: "password"
     product = Product.create! name: "Some Product"
     auction = Auction.create! value: 10, product_id: product.id
@@ -10,6 +12,7 @@ class PlaceBidTest < MiniTest::Test
     # placing the bid
     # instantiate a new service to place bid
     # insert a new bid value, attach to the user id of another user, and add auction id
+    # value, user_id, auction_id needs to be defined in lib/place_bid.rb
     service = PlaceBid.new value: 11, user_id: another_user, auction_id: auction.id
 
     service.execute
